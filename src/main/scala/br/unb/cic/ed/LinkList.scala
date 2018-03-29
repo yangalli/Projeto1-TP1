7647b2e5d921ade3c05e2d46fce8c702e541eec0
Yan Galli <yangalli@cjr.org.br>
2018-03-29 19:07:52 -0300

    Array List, Linked List and Array Stack done

--- src/main/scala/br/unb/cic/ed/LinkList.scala
+++
@@ -1,8 +0,0 @@
-package br.unb.cic.ed
-
-trait LinkList[A] {
-  def find(value: A): Option[Int]
-  def update(pos: Int, value: A ): Unit
-  def insert(pos: Int, value: A): Unit
-  //def remove(pos: Int): Unit
-}
